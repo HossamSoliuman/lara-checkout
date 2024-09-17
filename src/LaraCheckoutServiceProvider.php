d42f08f198e486ca780d8775a4b15dafff2b02a7
Hossam <hossamsoliuman@gmail.com>
2024-09-17 18:44:10 +0300

    init

---
+++ src/LaraCheckoutServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Hossam\LaraCheckout;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaraCheckoutServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        // Register your package's services here
+    }
+
+    public function boot()
+    {
+        // Perform package bootstrapping here (e.g., routes, migrations, views)
+    }
+}
